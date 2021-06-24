45df728db22dfe5300f27fbbd73de36c97b9776e
AlexDorobantu <alex_d1993@yahoo.es>
2021-06-24 18:20:38 +0200

    TAP2

---
+++ Tap2_CSharp/solution_bstree2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace bstree2
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            BSTree<int> arbol = new BSTree<int>();
+            arbol.Insert(5);
+            arbol.Insert(3);
+            arbol.Insert(6);
+            arbol.Insert(1);
+            arbol.Insert(4);
+            Console.WriteLine(arbol.Contains(4));
+            Console.WriteLine(arbol.Contains(8));
+
+            foreach (int elem in arbol)
+            {
+                Console.WriteLine(elem);
+            }
+
+        }
+    }
+
+}
